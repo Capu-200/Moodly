53ac4fb8ab880966d983f78b3a4090569c13da2e
Capu <capucine.keates@epitech.digital>
2023-12-19 10:45:47 +0100

    delete du back

--- back-strapi/src/api/raison/controllers/raison.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * raison controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::raison.raison');
--- back-strapi/src/api/raison/routes/raison.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * raison router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::raison.raison');
