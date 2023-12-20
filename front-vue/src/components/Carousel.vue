c5cb7b75769b7ed13b30c90bd3f5e0efff365b2b
Capu <capucine.keates@epitech.digital>
2023-12-20 12:28:07 +0100

    [update]Avancées Récupération des données en dynamique pour les moods

--- front-vue/src/components/Carousel.vue
+++ front-vue/src/components/Carousel.vue
@@ -1,3 +1,61 @@
+<script>
+import axios from 'axios';
+
+const token = '5cb7857127429627d59e735594d07ee35651e94cd950b75289714c650af667fade52a57f7a7cde758db8a0db03d9b288428481fe66fce08a3c28e5e2161643539c974cd11862af8d66f95f54b6837ab485128f90857b114d7e21c6fee148de22cf6600131d2a66710ab753883536bcf222c4cd15682488bcca0e7c978a01ffc2';
+
+export default {
+    data() {
+        return {
+            currentEmojiIndex: 0,
+        };
+    },
+    methods: {
+        updateCarousel() {
+            const offset = -100 * this.currentEmojiIndex;
+            this.$refs.carouselContainer.style.transform = `translateX(${offset}%)`;
+            this.displayCurrentMood();
+        },
+        displayCurrentMood() {
+            // Assurez-vous que le carrousel a des enfants et que l'index est valide
+            if (this.$refs.carouselContainer.children.length > 0 && this.currentEmojiIndex < this.$refs.carouselContainer.children.length) {
+                const currentMoodElement = this.$refs.carouselContainer.children[this.currentEmojiIndex];
+                const currentMoodText = currentMoodElement.querySelector('p').textContent;
+                console.log('Humeur actuelle:', currentMoodText);
+            }
+        }
+    },
+    mounted() {
+        this.$refs.prevBtn.addEventListener('click', () => {
+            const totalEmojis = this.$refs.carouselContainer.children.length;
+            this.currentEmojiIndex = (this.currentEmojiIndex - 1 + totalEmojis) % totalEmojis;
+            this.updateCarousel();
+        });
+
+        this.$refs.nextBtn.addEventListener('click', () => {
+            const totalEmojis = this.$refs.carouselContainer.children.length;
+            this.currentEmojiIndex = (this.currentEmojiIndex + 1) % totalEmojis;
+            this.updateCarousel();
+        });
+
+        // Request API.
+        axios
+        .get('http://localhost:1337/api/moods', {
+            headers: {
+            Authorization: `Bearer ${token}`,
+            },
+        })
+        .then(response => {
+            // Handle success.
+            console.log('Data: ', response.data);
+        })
+        .catch(error => {
+            // Handle error.
+            console.log('An error occurred:', error.response);
+        });
+    }
+};
+</script>
+
 <template>
     <div class="flex items-center justify-center h-full">
         <button ref="prevBtn" class="text-black font-bold py-2 px-4 rounded">
@@ -47,41 +105,3 @@
         </button>
     </div>
 </template>
-
-<script>
-export default {
-    data() {
-        return {
-            currentEmojiIndex: 0,
-        };
-    },
-    methods: {
-        updateCarousel() {
-            const offset = -100 * this.currentEmojiIndex;
-            this.$refs.carouselContainer.style.transform = `translateX(${offset}%)`;
-            this.displayCurrentMood();
-        },
-        displayCurrentMood() {
-            // Assurez-vous que le carrousel a des enfants et que l'index est valide
-            if (this.$refs.carouselContainer.children.length > 0 && this.currentEmojiIndex < this.$refs.carouselContainer.children.length) {
-                const currentMoodElement = this.$refs.carouselContainer.children[this.currentEmojiIndex];
-                const currentMoodText = currentMoodElement.querySelector('p').textContent;
-                console.log('Humeur actuelle:', currentMoodText);
-            }
-        }
-    },
-    mounted() {
-        this.$refs.prevBtn.addEventListener('click', () => {
-            const totalEmojis = this.$refs.carouselContainer.children.length;
-            this.currentEmojiIndex = (this.currentEmojiIndex - 1 + totalEmojis) % totalEmojis;
-            this.updateCarousel();
-        });
-
-        this.$refs.nextBtn.addEventListener('click', () => {
-            const totalEmojis = this.$refs.carouselContainer.children.length;
-            this.currentEmojiIndex = (this.currentEmojiIndex + 1) % totalEmojis;
-            this.updateCarousel();
-        });
-    }
-};
-</script>
--- front-vue/src/views/Login.vue
+++ front-vue/src/views/Login.vue
@@ -38,7 +38,7 @@
           </div>
   
           <div>
-            <button type="submit" class="flex w-full justify-center rounded-md bg-rose-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Se connecter</button>
+            <button type="submit" class="flex w-full justify-center rounded-md bg-rose-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-rose-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Se connecter</button>
           </div>
         </form>
       </div>
